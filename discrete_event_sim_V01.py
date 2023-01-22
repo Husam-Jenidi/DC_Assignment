e5ce0b6d3ce4447764146fbfd736617adf6ee06b
Husam Jenidi <117489131+Husam-Jenidi@users.noreply.github.com>
2023-01-23 00:48:49 +0100

    new

--- discrete_event_sim_V01.py
+++ discrete_event_sim_V01.py
@@ -71,20 +71,8 @@ class Event:
 
     def process(self, sim1):
         sim1.log_info(f'Started {self.name}')
-        # sim1.schedule(self.duration, EndEvent(self.name, self.callback))
         self.callback()
         sim1.log_info(f'Ended {self.name}')
-
-#
-# class EndEvent(Event):
-#     def __init__(self, name, callback):
-#         super().__init__(name, 0, callback)
-#
-#     def process(self, sim1):
-#         sim1.log_info(f'Ended {self.name}')
-
-    # raise NotImplementedError, it didn't work
-    # Example of how to use the simulation
 
 
 def my_callback():
